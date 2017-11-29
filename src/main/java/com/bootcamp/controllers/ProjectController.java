505fad339d34e7a0ccf1bd54bb1c39fd60123968
tdarex <tdarex@asklytics.io>
2017-11-29 09:51:39 +0100

    first commit

---
+++ src/main/java/com/bootcamp/controllers/ProjectController.java
@@ -0,0 +1,7 @@
+package com.bootcamp.controllers;
+
+/**
+ * Created by darextossa on 11/29/17.
+ */
+public class ProjectController {
+}
---
+++ src/main/java/com/bootcamp/services/PilierService.java
@@ -0,0 +1,11 @@
+package com.bootcamp.services;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by darextossa on 11/29/17.
+ */
+
+@Component
+public class PilierService {
+}
